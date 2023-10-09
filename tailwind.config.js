ec28fe208e43c09392a713f76838c0a1a19d335f
nice9uy <abdulrachman.mails@gmail.com>
2023-10-09 13:09:27 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // "./src/**/*.{html,js}",
+    "./templates/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
